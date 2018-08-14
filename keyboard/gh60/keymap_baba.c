800c1ff8beb73916e91136ebfb12678956258d93
scinart <akubeej@gmail.com>
2018-08-14 00:35:23 -0400

    update baba's keyboard

--- keyboard/gh60/keymap_baba.c
+++ keyboard/gh60/keymap_baba.c
@@ -11,9 +11,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------.
      * |ESC | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
      * |-----------------------------------------------------------|
-     * |TAB  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
+     * |FN0/-|  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
      * |-----------------------------------------------------------|
-     * |FN0   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
+     * |GRV    | A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
      * |-----------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|   Up     |
      * |-----------------------------------------------------------|
@@ -22,8 +22,8 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     KEYMAP_ANSI(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-        TAB ,Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        FN0, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
+        GRV, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          UP, \
         LCTL,LGUI,LALT,          SPC,                     RALT, LEFT,DOWN,RGHT),
 
@@ -31,9 +31,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------.
      * |`   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|DEL   |
      * |-----------------------------------------------------------|
-     * |CAPS |   |   |   |   |   |   |⇤ | ↑ | ⇥ | ⤒ |   |  | SPC |
+     * |     |   |   |   |   |   |   |⇤ | ↑ | ⇥ | ⤒ |   |  | SPC |
      * |-----------------------------------------------------------|
-     * |      | V0| V-| V+|   |   |    |← | ↓ |→ |⤓  |   |Return |
+     * |CAPS  | V0| V-| V+|   |   |    |← | ↓ |→ |⤓  |   |Return |
      * |-----------------------------------------------------------|
      * |Shift  |⟸| ⏸ |⟹|   |   |    |   |    |MENU|   |   Up   |
      * |-----------------------------------------------------------|
@@ -41,11 +41,11 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     KEYMAP_ANSI(
-        GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,  \
-        CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,HOME, UP ,END ,PGUP,TRNS,TRNS,SPC,  \
-        TRNS,MUTE,VOLD,VOLU,TRNS,TRNS,TRNS,LEFT,DOWN,RGHT,PGDN,TRNS,     TRNS,  \
-        TRNS,MPRV,MPLY,MNXT,TRNS,TRNS,TRNS,TRNS,TRNS,MENU, TRNS,          TRNS, \
-        TRNS,TRNS,TRNS,          TRNS,                     TRNS, TRNS,TRNS,TRNS),
+        TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,  \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,HOME, UP ,END ,PGUP,TRNS,TRNS, TRNS, \
+        CAPS,MUTE,VOLD,VOLU,TRNS,TRNS,TRNS,LEFT,DOWN,RGHT,PGDN,TRNS,      TRNS, \
+        TRNS,MPRV,MPLY,MNXT,TRNS,TRNS,TRNS,TRNS,TRNS,MENU,TRNS,           TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS, TRNS,TRNS,TRNS),
 };
 
 /*
@@ -57,7 +57,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_ON_OFF(1),
+    [0] = ACTION_LAYER_TAP_KEY(1, KC_TAB),
 };
 
 #ifdef KEYMAP_IN_EEPROM_ENABLE
